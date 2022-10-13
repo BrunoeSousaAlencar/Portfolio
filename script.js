12725efbd730b04b8db40f6c9e7a1830019085c0
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-10-13 18:01:20 -0300

    animação do texto

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function menuShow(){
         ul.classList.add('open');
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    new TypeIt(".animated", {
+        speed: 200,
+        loop: true
+    }).type("Bruno Alencar", {delay: 900}).delete(13).type("Programador", {delay: 500}).delete(12).type("Freelancer", {delay: 900}).delete(12).type("Web Developer", {delay: 900}).delete(12)
+    .go()
+});
